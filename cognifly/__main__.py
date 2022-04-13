a78ff9083a3f9ebb61583cbb7ee0f32a0b1153ce
Yann Bouteiller <yann.bouteiller@polymtl.ca>
2022-04-12 20:29:42 -0400

    test

--- cognifly/__main__.py
+++ cognifly/__main__.py
@@ -10,9 +10,13 @@ from cognifly.cognifly_controller.cognifly_controller import run_controller
 
 _, cmd, *args = sys.argv
 
-if cmd == "controller":
-    run_controller()
-elif cmd == "controller-headless":
-    run_controller(print_screen=False)
-else:
-    raise AttributeError("Undefined command: " + cmd)
+try:
+    if cmd == "controller":
+        run_controller()
+    elif cmd == "controller-headless":
+        run_controller(print_screen=False)
+    else:
+        raise AttributeError("Undefined command: " + cmd)
+except Exception as e:
+    logging.info(f"the following exception occurred:\n{e}")
+    raise e
