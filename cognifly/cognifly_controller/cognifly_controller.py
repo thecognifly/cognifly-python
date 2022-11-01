ce4d9f350d1add3946733924b6c7c2bcb496db8f
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-10-31 20:54:31 -0400

    test

--- cognifly/cognifly_controller/cognifly_controller.py
+++ cognifly/cognifly_controller/cognifly_controller.py
@@ -833,8 +833,7 @@ class CogniflyController:
             pos_x_wf, pos_y_wf, pos_z_wf, yaw, vel_x_wf, vel_y_wf, vel_z_wf, yaw_rate = self._read_estimate(board)
         else:
             if self.custom_gps:
-                pass
-                # set_gps_from_xyz(board=board, x=pos_x_wf, y=pos_y_wf, z=pos_z_wf)
+                set_gps_from_xyz(board=board, x=pos_x_wf, y=pos_y_wf, z=pos_z_wf)
 
         old_pos_x_wf = pos_x_wf
         old_pos_y_wf = pos_y_wf
