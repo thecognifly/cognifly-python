3a9872dc3c82c8d5d3406c222c083348362e48b1
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-11-24 15:37:48 -0500

    debug

--- cognifly/cognifly_controller/cognifly_controller.py
+++ cognifly/cognifly_controller/cognifly_controller.py
@@ -922,7 +922,8 @@ class CogniflyController:
                 try_addstr(screen, 28, 0, f"FC internal estimates:")
                 try_addstr(screen, 29, 0, f"pos: [{pos_x_wf: .5f},{pos_y_wf: .5f},{pos_z_wf: .5f}] m, yaw: {yaw: .5f} rad")
                 try_addstr(screen, 30, 0, f"vel: [{vel_x_wf: .5f},{vel_y_wf: .5f},{vel_z_wf: .5f}] m/s, w: {yaw_rate: .5f} rad/s")
-                try_addstr(screen, 31, 0, f"nav_epv: {nav_epv}, nav_eph: {nav_eph}, flags: {bin(flags)}")
+                try_addstr(screen, 31, 0, f"nav_epv: {nav_epv}, nav_eph: {nav_eph}")
+                try_addstr(screen, 32, 0, f"flags: {bin(flags)}")
 
         except Exception as e:
             return None, None, None, None, None, None, None, None
