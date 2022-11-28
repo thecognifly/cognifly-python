ee5fe723ce7c9dbe491a97fe0cf00207c472dc1f
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2022-11-28 16:33:40 -0500

    test

--- cognifly/cognifly_controller/cognifly_controller.py
+++ cognifly/cognifly_controller/cognifly_controller.py
@@ -1351,7 +1351,7 @@ class CogniflyController:
             if self.print_screen:
                 try_addstr(screen, 15, 0, "Connecting to the FC...")
 
-            with MSPy(device=self.device_str, loglevel='WARNING', baudrate=115200, timeout=0, min_time_between_writes=1/50) as board:
+            with MSPy(device=self.device_str, loglevel='WARNING', baudrate=115200, timeout=0, min_time_between_writes=1/100) as board:
                 if board == 1:  # an error occurred...
                     return 1
                 else:
