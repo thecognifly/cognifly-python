4c682cfb34477dfd992f9374b827322d5201e3ec
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2023-03-17 00:59:25 -0400

    blocking by default

--- cognifly/utils/udp_interface.py
+++ cognifly/utils/udp_interface.py
@@ -38,16 +38,15 @@ class UDPInterface(object):
         while clean_buffer:
             logging.debug(f"Cleaning receiving buffer...")
             try:
+                self.__sockI.setblocking(False)
                 if not WINDOWS:
                     self.__sockI.recv(1, socket.MSG_DONTWAIT)
                 else:
-                    self.__sockI.setblocking(False)
                     self.__sockI.recv(1)
             except IOError:  # recv raises a error when no data is received
                 clean_buffer = False
             finally:
-                if WINDOWS:
-                    self.__sockI.setblocking(True)
+                self.__sockI.setblocking(True)
         logging.debug(f"Cleaning receiving buffer...Done!")
 
     def __del__(self):
@@ -95,16 +94,15 @@ class UDPInterface(object):
         res = []
         while True:
             try:
+                self.__sockI.setblocking(False)
                 if not WINDOWS:
                     data, _ = self.__sockI.recvfrom(self.__buffersize, socket.MSG_DONTWAIT)
                 else:
-                    self.__sockI.setblocking(False)
                     data, _ = self.__sockI.recvfrom(self.__buffersize)
             except IOError:
                 return res
             finally:
-                if WINDOWS:
-                    self.__sockI.setblocking(True)
+                self.__sockI.setblocking(True)
             res += [data]
 
 
