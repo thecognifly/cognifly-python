de013684c3c77a8b1cfe73df5373e2579368c168
Yann Bouteiller <yann.bouteiller@hotmail.fr>
2023-05-04 17:00:09 -0400

    hover on arming

--- cognifly/cognifly_controller/cognifly_controller.py
+++ cognifly/cognifly_controller/cognifly_controller.py
@@ -50,7 +50,7 @@ KEY_TIMEOUT = 0.2  # seconds before a keyboard command times out
 
 DEFAULT_ROLL = 1500
 DEFAULT_PITCH = 1500
-DEFAULT_THROTTLE = 1500  # throttle bellow a certain value disarms the FC
+DEFAULT_THROTTLE = 1000  # throttle bellow a certain value disarms the FC
 DEFAULT_YAW = 1500
 
 TAKEOFF = 1400
@@ -828,6 +828,7 @@ class CogniflyController:
 
     def _complete_arming(self, board):
         self.CMDS['aux2'] = NAV_POSHOLD_MODE
+        self.CMDS['throttle'] = PH_HOVER
         self._send_cmds(board)
         self._armed = True
         self._arming = False
